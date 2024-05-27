961e3cd94166c531f9c145462e8bf10089f51d7d
sivaramakrishnan0 <sivaramakrishnan0327@gmail.com>
2024-05-27 22:04:19 +0530

    Updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Home.css'; // Import the CSS file
+import './Home.css';
+import vectorImage from '../Images/home-img.png'; // Ensure you have the correct path to your image
+import logoImage from '../Images/logo.png'; // Ensure you have the correct path to your logo image
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleLoginNavigation = () => {
-    navigate('/login');
-  };
-
   return (
-    <div>
-      <h1>Welcome to eCampus</h1>
-      <button onClick={handleLoginNavigation}>Go to Login</button>
-    </div>
+    <>
+      <header className="navbar">
+        <div className="logo">
+          <img src={logoImage} alt="Logo" className="logo-image" />
+        </div>
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="https://www.kgkite.ac.in" target="_blank" rel="noopener noreferrer">About</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </header>
+      <div className="home-container">
+        <div className="image-section">
+          <img src={vectorImage} alt="Vector" className="vector-image" />
+        </div>
+        <div className="welcome-section">
+          <h1>Welcome To Ecampus</h1>
+          <div className="login-buttons">
+            <button onClick={() => window.location.href = '/faculty-login'}>Faculty Login</button>
+            <button onClick={() => window.location.href = '/student-login'}>Student Login</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/auth/connection.js
+++ src/auth/connection.js
@@ -1,2 +1,2 @@
-const baseURL = 'http://192.168.78.131:5000';
+const baseURL = 'http://192.168.0.108:5000';
 export default baseURL;
